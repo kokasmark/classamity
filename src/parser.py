62f392321ec124fbd185017b11d7903ddb57a8f6
Kokas Márk <112559972+kokasmark@users.noreply.github.com>
2025-01-18 12:23:23 +0100

    Parser

--- src/parser.py
+++ src/parser.py
@@ -2,14 +2,12 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-# Function to count occurrences of the word in the page content
 def count_word_occurrences(url, word):
     response = requests.get(url)
     text = str(response.content).lower()
     word_count = text.count(word.lower())
     return word_count
 
-# Function to add attributes if the threshold is exceeded
 def add_attribute_to_weapons(data, words, threshold=3):
     for stage in data:
         print(stage['title'])
@@ -26,17 +24,13 @@ def add_attribute_to_weapons(data, words, threshold=3):
                             weapon['attributes'] = []
                         weapon['attributes'].append(word)
 
-# Load the data.json file
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-# List of words to search for
 search_words = ["homing", "piercing"]
 
-# Run the function to update the weapons data
 add_attribute_to_weapons(data, search_words)
 
-# Save the updated data back to the file (optional)
 with open('updated_data.json', 'w') as file:
     json.dump(data, file, indent=4)
 
